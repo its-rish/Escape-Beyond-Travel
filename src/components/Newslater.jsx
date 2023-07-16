7a4ab5f1c5873d5555d27c95976108e7dfc3e472
its-rish <79601615+its-rish@users.noreply.github.com>
2023-07-16 23:41:13 +0530

    minor changes on news-later in home page

--- src/components/Newslater.jsx
+++ src/components/Newslater.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import "../styles/newslater.scss"
 import NewsLaterImg from "../assets/NewsLater.png"
+import NewsLaterDesign from "../assets/design-wrapper.svg"
 
 function Newslater() {
   return (
     <div className='newslater'>
-    <div className="container">
-      <div className="wrap-container">
-      <div className="left-side">
+      <div className="container">
+        <div className="wrap-container">
+          <div className="left-side">
             <div className="text-wraper">
-            <h2>Get started today</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitusce convallis</p>
-            <div className="form">
-             <div className="input-text">
-             <input type="text"  placeholder='First name'/>
-                <input type="text " placeholder='Last name' />
-                <input type="number" placeholder='Phone' />
-                <input type="email" placeholder='Email'/>
-             </div>
-                <textarea placeholder='Message'className='text-area'></textarea>
+              <h2>Get started today</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitusce convallis</p>
+              <div className="form">
+                <div className="input-text">
+                  <input type="text" placeholder='First name' />
+                  <input type="text " placeholder='Last name' />
+                  <input type="number" placeholder='Phone' />
+                  <input type="email" placeholder='Email' />
+                </div>
+                <textarea placeholder='Message' className='text-area'></textarea>
                 <button className="global-btn">Submit now</button>
+              </div>
             </div>
-            </div>
-        </div>
-        <div className="right-side">
+
+          </div>
+          <div className="right-side">
             <div className="img-wraper">
-            <img src={NewsLaterImg} alt="img" />
+              <img src={NewsLaterImg} alt="img" />
             </div>
+          </div>
+
         </div>
       </div>
-    </div>
+      <div className="design-wrapper">
+        <img src={NewsLaterDesign} alt="" />
+      </div>
     </div>
   )
 }
